beaeee52df0857de32f4456107a814e2236befd7
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-16 22:28:28 +0200

    added linter

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,18 @@
-//swapi.co
+// swapi.co
 
-const getResource = async (url) => {
-    const res = await fetch(url);
-    const body = await res.json();
-    return body;
-}
+const getResource = async url => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
 
-getResource('https://swapi.co/api/people/1').then((body)=>console.table('body', body));
+getResource('https://swapi.co/api/people/1').then(body =>
+  console.table('body', body)
+);
 
 /*
 fetch('https://swapi.co/api/people/1').then((res) => {
     return res.json()
 }).then((body) => {
     console.table('body', body)
-});*/
+}); */

6b0151266551bf160811258db62092180658c6e0
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-16 22:36:51 +0200

    handled request when error

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,17 @@
 
 const getResource = async url => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
   const body = await res.json();
   return body;
 };
 
-getResource('https://swapi.co/api/people/1').then(body =>
-  console.table('body', body)
-);
+getResource('https://swapi.co/api/people/1')
+  .then(body => console.table('body', body))
+  .catch(err => console.error(`Could not fetch ${err}`));
 
 /*
 fetch('https://swapi.co/api/people/1').then((res) => {

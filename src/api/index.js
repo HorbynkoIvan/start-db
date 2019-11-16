04d30a629eca7f852f0c879402f7dfdcfd0edf02
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-16 21:40:21 +0200

    rebuild request using async await

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,16 @@
 //swapi.co
 
-fetch('https://swapi.co/api/people/1').then((res)=>{
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+}
+
+getResource('https://swapi.co/api/people/1').then((body)=>console.table('body', body));
+
+/*
+fetch('https://swapi.co/api/people/1').then((res) => {
     return res.json()
-}).then((body)=>{
+}).then((body) => {
     console.table('body', body)
-})
+});*/

6adb66f1d03e43b3be4b1983119040e15fd15c23
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-17 19:52:23 +0200

    upgrade request api, created class SwapiApi

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,61 @@
 // swapi.co
 
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(
+        `Could not fetch ${this._apiBase}${url}, received ${res.status}`
+      );
+    }
+    return res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource('/people/');
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource('/planets/');
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
+
+  async getAllStarShips() {
+    const res = await this.getResource('/starships/');
+    return res.results;
+  }
+
+  getStarShip(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then(people => {
+  people.forEach(person => console.log(person.name));
+});
+
+swapi.getPerson(3).then(person => {
+  console.log(person.name);
+});
+
+
+
+
+
+/* 2)
 const getResource = async url => {
   const res = await fetch(url);
 
@@ -12,9 +68,9 @@ const getResource = async url => {
 
 getResource('https://swapi.co/api/people/1')
   .then(body => console.table('body', body))
-  .catch(err => console.error(`Could not fetch ${err}`));
+  .catch(err => console.error(`Could not fetch ${err}`)); */
 
-/*
+/* 1)
 fetch('https://swapi.co/api/people/1').then((res) => {
     return res.json()
 }).then((body) => {

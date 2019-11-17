4e490c014b4493f3146ab1c63b4ed07391534b35
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-17 21:40:05 +0200

    PersonDetails markup

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -2,11 +2,28 @@ import React from 'react';
 import './person-details.css';
 
 function PersonDetails() {
-    return (
-        <div>
-            PersonDetails
-        </div>
-    );
+  return (
+    <div className="person-details card">
+      <img src="" className="person-image" alt="..." />
+      <div className="card-body">
+        <h4>R2-D2</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>male</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Birth Year</span>
+            <span>43</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye color</span>
+            <span>red</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default PersonDetails;

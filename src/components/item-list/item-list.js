6e80e410154b938dcabbcb156721bed3581c87bc
HorbynkoIvan <horbynko.i@gmail.com>
2019-11-18 08:39:00 +0200

    RandomPlanet connection api

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './item-list.css';
 
 function ItemList() {
-    return (
-        <ul className='item-list list-group'>
-            <li className='list-group-item'>1</li>
-            <li className='list-group-item'>2</li>
-            <li className='list-group-item'>3</li>
-        </ul>
-    );
+  return (
+    <ul className="item-list list-group">
+      <li className="list-group-item">1</li>
+      <li className="list-group-item">2</li>
+      <li className="list-group-item">3</li>
+    </ul>
+  );
 }
 
 export default ItemList;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './random-planet.css';
+import SwapiService from '../../services';
 
-function RandomPlanet() {
+class RandomPlanet extends Component {
+    const swapi = new SwapiService();
+  state = {
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null,
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  updatePlanet() {
+      this.swapi.getPlanet(7).then((planet)=>{
+          this.setState({
+              name: planet.name,
+              population: planet.population,
+              rotationPeriod: planet.rotation_period,
+              diameter: planet.diameter,
+          })
+      })
+  }
+
+  render() {
+    const { name, population, rotationPeriod, diameter } = this.state;
     return (
-        <div>
-            RandomPlanet
+      <div className="random-planet jumbotron rounded">
+        <img src="" className="planet-image" alt="..." />
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
         </div>
+      </div>
     );
+  }
 }
 
 export default RandomPlanet;

f6a65f54e0e1f1c574301da7e2d407aa0916d157
HorbynkoIvan <horbynko.i@gmail.com>
2019-12-06 10:51:00 +0200

    added listener to itemlist, got id for persondetails

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <RandomPlanet />
-      <div className="row mb-2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
+  };
+
+  componentDidMount() {}
+
+  onPersonSelected(id) {
+    this.setState({
+      selectedPerson: id,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <RandomPlanet />
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

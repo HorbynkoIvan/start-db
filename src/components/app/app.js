bef4281a0c5c872bfad64fed9aca1bf45187f071
HorbynkoIvan <horbynko.i@gmail.com>
2019-12-09 22:27:41 +0200

    refactoring App, PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,36 +2,15 @@ import React, { Component } from 'react';
 import './app.css';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PeoplePage from '../people-page';
 
 class App extends Component {
-  state = {
-    selectedPerson: 5,
-  };
-
-  componentDidMount() {}
-
-  onPersonSelected = id => {
-    this.setState({
-      selectedPerson: id,
-    });
-  };
-
   render() {
-    const { selectedPerson } = this.state;
     return (
       <div className="container">
         <Header />
         <RandomPlanet />
-        <div className="row mb-2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './people-page.scss';
 import SwapiService from '../../services';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
 
 class PeoplePage extends Component {
-    swapi = new SwapiService();
+  swapi = new SwapiService();
 
-    render() {
-        return (
-            <div>PeoplePage</div>
-        );
-    }
+  state = {
+    selectedPerson: 5,
+  };
+
+  onPersonSelected = id => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+
+  render() {
+    const { selectedPerson } = this.state;
+    return (
+      <div className="row mb-2">
+        <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId={selectedPerson} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PeoplePage;

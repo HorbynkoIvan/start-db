c9df1970ac1a541aaabe3fdedaf0de7ff3bfb82a
Horbynko_Ivan <horbynko.i@gmail.com>
2019-12-22 18:20:13 +0200

    refactoring component StarshipDetails

--- src/components/sw-components/starship-details.js
+++ src/components/sw-components/starship-details.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { ItemDetails, Record } from '../item-details';
-import { SwapiServiceConcumer } from '../swapi-service-context';
+import { withSwapiService } from '../hoc-helpers';
 
-export const StarshipDetails = ({ itemId }) => (
-  <SwapiServiceConcumer>
-    {({ getStarShip, getStarShipImage }) => (
-      <ItemDetails
-        itemId={itemId}
-        getData={getStarShip}
-        getImageUrl={getStarShipImage}
-      >
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-      </ItemDetails>
-    )}
-  </SwapiServiceConcumer>
+const StarshipDetails = props => (
+  <ItemDetails {...props}>
+    <Record field="model" label="Model" />
+    <Record field="length" label="Length" />
+    <Record field="costInCredits" label="Cost" />
+  </ItemDetails>
 );
+
+const mapMethodsToProps = swapiService => ({
+  getData: swapiService.getStarShip,
+  getImageUrl: swapiService.getStarShipImage,
+});
+
+export default withSwapiService(StarshipDetails, mapMethodsToProps);

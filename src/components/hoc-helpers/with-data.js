7f194d3af28db1986ab4d5302784a3d5787cdb1e
Horbynko_Ivan <horbynko.i@gmail.com>
2019-12-25 19:26:26 +0200

    refactoring withdata

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Spinner from '../spinner';
+import ErrorIndicator from "../error-indicator";
 
 const withData = View =>
   class extends Component {
     state = {
       data: null,
+      loading: true,
+      error: false,
     };
 
     componentDidMount() {
@@ -12,12 +15,22 @@ const withData = View =>
     }
 
     upDate() {
-      this.props.getData().then(data => this.setState({ data }));
+      this.setState({
+        loading: true,
+        error: false,
+      });
+      this.props
+        .getData()
+        .then(data => this.setState({ data, loading: false }))
+        .catch(() => {
+          this.setState({ error: true, loading: false });
+        });
     }
 
     render() {
-      const { data } = this.state;
-      if (!data) return <Spinner />;
+      const { data, loading, error } = this.state;
+      if (loading) return <Spinner />;
+      if (error) return <ErrorIndicator />;
       return <View {...this.props} data={data} />;
     }
   };

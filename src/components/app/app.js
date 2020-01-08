c8c4d1acf8f4533aeeb1e5f73a943ece26ac2afa
Horbynko Ivan <horbynko.i@brights.cloud>
2020-01-08 12:15:57 +0200

    rebuild App to functional component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,41 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './app.css';
 import Header from '../header';
 import SwapiService from '../../services';
 import ErrorBoundry from '../error-boundry';
-import {
-  PersonDetails,
-  PlanetDetails,
-  StarshipDetails,
-  PersonList,
-  PlanetList,
-  StarshipList,
-} from '../sw-components';
-import Row from '../row';
+
 import { SwapiServiceProvider } from '../swapi-service-context';
 import RandomPlanet from '../random-planet';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
-class App extends Component {
-  swapi = new SwapiService();
-
-  render() {
-    return (
-      <div className="container">
-        <ErrorBoundry>
-          <SwapiServiceProvider value={this.swapi}>
-            <div className="stardb-app">
-              <Header />
-              <RandomPlanet />
-              <PeoplePage />
-              <PlanetsPage />
-              <StarshipsPage />
-            </div>
-          </SwapiServiceProvider>
-        </ErrorBoundry>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const swapi = new SwapiService();
+  return (
+    <div className="container">
+      <ErrorBoundry>
+        <SwapiServiceProvider value={swapi}>
+          <div className="stardb-app">
+            <Header />
+            <RandomPlanet />
+            <PeoplePage />
+            <PlanetsPage />
+            <StarshipsPage />
+          </div>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
+    </div>
+  );
+};
 
 export default App;

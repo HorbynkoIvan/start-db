24d1802fa58c8f1657dc65f9ae436c4232985fc2
Horbynko Ivan <horbynko.i@brights.cloud>
2020-01-10 13:15:42 +0200

    added dinamic route to starships

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,7 @@ import ErrorBoundry from '../error-boundry';
 import { SwapiServiceProvider } from '../swapi-service-context';
 import RandomPlanet from '../random-planet';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import StarshipDetails from '../sw-components/starship-details';
 
 const App = () => {
   const swapi = new SwapiService();
@@ -23,7 +24,14 @@ const App = () => {
               <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route
+                path="/starships/:id"
+                render={({ match }) => {
+                  const { id } = match.params;
+                  return <StarshipDetails itemId={id} />;
+                }}
+              />
             </div>
           </Router>
         </SwapiServiceProvider>

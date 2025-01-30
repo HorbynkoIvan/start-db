b0f62e01544a93ebefb4034088a1545359b81e67
Ivan Horbynko <horbynko.i@gmail.com>
2025-01-30 10:05:50 +0200

    render all films with images

--- src/pages/FilmsPage.jsx
+++ src/pages/FilmsPage.jsx
@@ -2,6 +2,7 @@ import React  from "react";
 import { useSwapi } from "@/context";
 import { Spinner } from "@components/Spinner";
 import { useSwapiResource } from "@/hooks";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 // episode_id
 export const FilmsPage = () => {
@@ -14,7 +15,20 @@ export const FilmsPage = () => {
   return (
     <div>
       <ul>
-        {films.map((film) => (<li key={film.episode_id}>{film.title}</li>))}
+        {films.map((film) => (
+          <Card key={film.episode_id} sx={{ backgroundColor: "#1e1e1e", color: "#ffffff" }}>
+          <CardMedia
+            component="img"
+            height="300"
+            image={`https://starwars-visualguide.com/assets/img/films/${film.episode_id}.jpg`}
+            alt={film.title}
+          />
+          <CardContent>
+            <Typography variant="h6">{film.title}</Typography>
+            <Typography variant="body2">Episode {film.episode_id}</Typography>
+            <Typography variant="body2">Release Date: {film.release_date}</Typography>
+          </CardContent>
+        </Card>))}
       </ul>
     </div>
   );
